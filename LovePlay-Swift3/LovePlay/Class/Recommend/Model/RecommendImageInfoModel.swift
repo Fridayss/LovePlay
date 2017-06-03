cdbe858733ef44b67b6203ed02413f15f98b0c1f
YunsChou <2647754496@qq.com>
2017-06-03 17:07:48 +0800

    solve swift conflic

--- LovePlay-Swift3/LovePlay/Class/Recommend/Model/RecommendImageInfoModel.swift
+++ LovePlay-Swift3/LovePlay/Class/Recommend/Model/RecommendImageInfoModel.swift
@@ -2,22 +2,13 @@
 //  RecommendImageInfoModel.swift
 //  LovePlay
 //
-<<<<<<< HEAD
 //  Created by Yuns on 2017/5/30.
-=======
-//  Created by weiying on 2017/5/27.
->>>>>>> 1875739c6fa8c4890ae7b045ac14824f393cd618
-//  Copyright © 2017年 yuns. All rights reserved.
-//
+
 
 import UIKit
 import HandyJSON
 
 class RecommendImageInfoModel: HandyJSON {
-<<<<<<< HEAD
-=======
-
->>>>>>> 1875739c6fa8c4890ae7b045ac14824f393cd618
     var address : String?
     var docid : String?
     var imgUrl : String?
